19914ea2fea9d66e2c9e081f610a0fdc8ae3a107
Zhang YingJie <zhangyj@stpass.com>
2024-01-11 11:11:53 +0800

    feat: save

--- packages/base/src/pages/login/index.vue
+++ packages/base/src/pages/login/index.vue
@@ -11,6 +11,11 @@ export default {
 </script>
 <script setup lang='ts'>
 import {ref} from 'vue'
+import { vueStore } from '@hoslink/store';
+const store: any = vueStore() // 字典仓库
+const {xxx} = store // 货品类型字典
+store.setXxx(123)
+console.log(store, 'vueStore')
 </script>
 
 <style scoped>
--- packages/store/index.ts
+++ packages/store/index.ts
@@ -1,14 +1,6 @@
-/*
- * @Author: kevinZzzzzz
- * @Date: 2023-05-11 09:30:47
- * @version:
- * @LastEditors: kevinZzzzzz
- * @LastEditTime: 2023-06-02 11:17:46
- * @Description: pinia状态管理器
- * @FilePath: \vue-ts-vite\src\store\index.ts
- */
-import { defineStore } from 'pinia'
-
+import { defineStore, createPinia} from 'pinia'
+import piniaPluginPersist from 'pinia-plugin-persist'
+createPinia().use(piniaPluginPersist)
 /*
   for example
 */
@@ -30,5 +22,9 @@ export const vueStore = defineStore({
     }
   },
   getters: {},
-  actions: {}
+  actions: {
+    setXxx(data: any) {
+      this.xxx = data
+    }
+  }
 })
